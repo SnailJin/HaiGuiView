48fc0d792d2a64689873bc34f9bf3f05550602b5
SnailJin <jin521436@163.com>
2016-04-18 23:05:25 +0800

    2016-04-18

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,5 +1,13 @@
 /**
  * Created by liguang.jin on 2016/3/31.
  */
-var url="http://47.89.38.171/HGZGZ/interface";
+var url="http://47.89.38.171/HGZGZ/";
+var formData = {
+    "body":{},
+    "code":0,
+    "key":"",
+    "message":"",
+    "time":new Date().getTime(),
+    "token":"",
+    "uuid":""}
 var rootApp =angular.module('rootApp',['ui.bootstrap']);
