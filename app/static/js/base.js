de41f6f66251a7f20e56bc72dc53f57797a1e8f7
unknown <liguang.jin@mopote.com>
2016-04-25 18:28:24 +0800

    t2016-4-25

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,7 +1,7 @@
 /**
  * Created by liguang.jin on 2016/3/31.
  */
-var url="http://47.89.38.171/HGZGZ/";
+var url="http://47.89.38.171/HGZGZ/interface";
 var formData = {
     "body":{},
     "code":0,
@@ -11,3 +11,9 @@ var formData = {
     "token":"",
     "uuid":""}
 var rootApp =angular.module('rootApp',['ui.bootstrap']);
+//µ¯¿ò²å¼þ
+rootApp.directive("poplayer",function() {
+    return {
+        template: '<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" ng-model="modal-title">{{modal-title}}</h4></div><div class="modal-body" ng-model="modal-body">{{modal-body}}</div><div class="modal-footer"><button type="button" class="btn btn-primary">È·¶¨</button></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->'
+    }
+});
